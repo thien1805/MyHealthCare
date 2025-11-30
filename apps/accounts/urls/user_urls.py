0d52af1347117ad30b7672b9db36d29f3eac08fd
Thiện <thien2005@MacBook-Air-cua-Thien.local>
2025-11-30 17:14:22 +0700

    Add azure-pipeline.yml file

--- apps/accounts/urls/user_urls.py
+++ apps/accounts/urls/user_urls.py
@@ -3,6 +3,6 @@ from apps.accounts.views import ProfileView
 
 
 urlpatterns = [
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('me/', ProfileView.as_view(), name='me'),
 ] 
 
